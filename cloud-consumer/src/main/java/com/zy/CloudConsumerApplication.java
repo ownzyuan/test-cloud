3de9238d4f861f131d1032ba1712f0a364cc5688
ownzyuan <1076949424@qq.com>
2021-04-15 14:45:13 +0800

    解决问题方式一

--- cloud-consumer/src/main/java/com/zy/CloudConsumerApplication.java
+++ cloud-consumer/src/main/java/com/zy/CloudConsumerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableFeignClients("com.zy.api")
 public class CloudConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudConsumerApplication.class, args);
--- cloud-consumer/src/main/java/com/zy/controller/ConsumerController.java
+++ cloud-consumer/src/main/java/com/zy/controller/ConsumerController.java
@@ -1,24 +1,14 @@
 package com.zy.controller;
 
 import com.zy.api.StrApi;
-import com.zy.entity.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerController {
-
-//    @Autowired
-//    private DataApi dataApi;
-//
-//    @PostMapping("/insert")
-//    public String insertData(@RequestBody UserDTO userDTO){
-//        return dataApi.insertData(userDTO);
-//    }
 
     @Autowired
     private StrApi strApi;
